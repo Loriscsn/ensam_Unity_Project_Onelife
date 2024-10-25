a2fb8f902d34be6ac70ff0a262551ad56c8658f0
Loriscsn <loris.cousin86@gmail.com>
2024-10-25 15:16:03 +0200

    boo + texture

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; // Assure-toi d'avoir cette ligne pour charger les scènes
 
 public class MainMenu : MonoBehaviour
 {
     // Fonction pour le bouton Start
     public void StartGame()
     {
-        // Charge la première scène du jeu
-        SceneManager.LoadScene("Loris"); // Assure-toi d'avoir ajouté la scène dans le build settings
+        // Charge la première scène du jeu (remplace "GameScene" par le nom exact de ta scène)
+        SceneManager.LoadScene("GameScene"); // Assure-toi que "GameScene" correspond bien à la scène de ton jeu
     }
 
     // Fonction pour le bouton Quit
     public void QuitGame()
     {
-        Debug.Log("Quit"); // Affiche dans la console pour les tests dans l'éditeur
-        Application.Quit(); // Quitte l'application (ne fonctionne pas dans l'éditeur)
+        Debug.Log("Quitter le jeu"); // Juste pour tester dans l'éditeur
+        Application.Quit(); // Quitte le jeu (ne fonctionne que dans un build)
     }
 }
